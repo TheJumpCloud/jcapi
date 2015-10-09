ab26191519d0c684caa86a46eea7f868a4b10162
James Brown <james@jumpcloud.com>
2015-10-09 13:13:33 -0600

    Initial checkin after testing complete

---
+++ examples/ExportSystemsToCSV/exportSystemsToCSV.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+	"github.com/TheJumpCloud/jcapi"
+	"os"
+)
+
+const (
+	apiUrl string = "https://console.jumpcloud.com/api"
+)
+
+func outFirst(data string) {
+	fmt.Printf("\"%s\"", data)
+}
+
+func out(data string) {
+	fmt.Printf(",\"%s\"", data)
+}
+
+func endLine() {
+	fmt.Printf("\n")
+}
+
+func main() {
+	apiKey := os.Getenv("JUMPCLOUD_APIKEY")
+	if apiKey == "" {
+		fmt.Printf("%s: Please run:\n\n\texport JUMPCLOUD_APIKEY=<your-JumpCloud-API-key>\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	jc := jcapi.NewJCAPI(apiKey, apiUrl)
+
+	// Grab all system users with their tags
+	systems, err := jc.GetSystems(true)
+	if err != nil {
+		fmt.Printf("Could not read system users, err='%s'\n", err)
+		return
+	}
+
+	outFirst("Id")
+	out("DisplayName")
+	out("HostName")
+	out("Active")
+	out("Instance ID")
+	out("OS")
+	out("OSVersion")
+	out("AgentVersion")
+	out("CreatedDate")
+	out("LastContactDate")
+	endLine()
+
+	for _, system := range systems {
+		outFirst(system.Id)
+		out(system.DisplayName)
+		out(system.Hostname)
+		out(fmt.Sprintf("%t", system.Active))
+		out(system.AmazonInstanceID)
+		out(system.Os)
+		out(system.Version)
+		out(system.AgentVersion)
+		out(system.Created)
+		out(system.LastContact)
+
+		for _, tag := range system.Tags {
+			out(tag.Name)
+		}
+
+		endLine()
+	}
+}
