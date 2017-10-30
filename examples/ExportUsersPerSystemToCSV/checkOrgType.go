9b4b54fd0a6dd37a7f1e705abeee034bcaa96537
Renaud Amar <renaud.amar@jumpcloud.com>
2017-10-30 17:29:00 -0600

    Convert ExportUsersPerSystemToCSV to use new jcapiv2 format

--- examples/ExportUsersPerSystemToCSV/checkOrgType.go
+++ examples/ExportUsersPerSystemToCSV/checkOrgType.go
@@ -1,11 +1,14 @@
 package main
 
-import jcapiv2 "github.com/TheJumpCloud/jcapi-go/v2"
+import (
+	"context"
+
+	jcapiv2 "github.com/TheJumpCloud/jcapi-go/v2"
+)
 
 // the following constants are used for API v2 calls:
 const (
 	apiKeyEnvVariable  = "JUMPCLOUD_APIKEY"
-	apiKeyHeader       = "x-api-key"
 	contentType        = "application/json"
 	accept             = "application/json"
 	searchLimit        = 100
@@ -14,18 +17,28 @@ const (
 
 // isGroupsOrg returns true if this org is groups enabled:
 func isGroupsOrg(urlBase string, apiKey string) (bool, error) {
-	// instantiate a new API object for User Groups:
-	userGroupsAPI := jcapiv2.NewUserGroupsApiWithBasePath(urlBase + "/v2")
-	userGroupsAPI.Configuration.APIKey[apiKeyHeader] = apiKey
+	// instantiate a new API client object:
+	client := jcapiv2.NewAPIClient(jcapiv2.NewConfiguration())
+	client.ChangeBasePath(urlBase + "/v2")
+
+	// set up the API key via context:
+	auth := context.WithValue(context.TODO(), jcapiv2.ContextAPIKey, jcapiv2.APIKey{
+		Key: apiKey,
+	})
+
+	// set up optional parameters:
+	optionals := map[string]interface{}{
+		"limit": int32(1), // limit the query to return 1 item
+	}
 	// in order to check for groups support, we just query for the list of User groups
 	// (we just ask to retrieve 1) and check the response status code:
-	_, res, err := userGroupsAPI.GroupsUserList(contentType, accept, "", "", 1, 0, "")
+	_, res, err := client.UserGroupsApi.GroupsUserList(auth, contentType, accept, optionals)
 
 	// check if we're using the API v1:
 	// we need to explicitly check for 404, since GroupsUserList will also return a json
 	// unmarshalling error (err will not be nil) if we're running this endpoint against
 	// a Tags org and we don't want to treat this case as an error:
-	if res.Response != nil && res.Response.StatusCode == 404 {
+	if res != nil && res.StatusCode == 404 {
 		return false, nil
 	}
 
@@ -35,7 +48,7 @@ func isGroupsOrg(urlBase string, apiKey string) (bool, error) {
 	}
 
 	// if we're using API v2, we're expecting a 200:
-	if res.Response.StatusCode == 200 {
+	if res.StatusCode == 200 {
 		return true, nil
 	}
 
